e5be4b353d3ae1cbd517ef39519dd1727cd22872
Tim Barrett <tim.barrett.jr@gmail.com>
2018-07-25 21:51:16 -0700

    Added more tests on input controller and added ui changes to storyboard

--- ToDo/Controller/InputViewController.swift
+++ ToDo/Controller/InputViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class InputViewController: UIViewController {
     
@@ -18,12 +19,56 @@ class InputViewController: UIViewController {
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     
+    lazy var geocoder = CLGeocoder()
+    var itemManager: ItemManager?
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    
+    @IBAction func save() {
+        guard let titleString = titleTextField.text,
+            titleString.count > 0 else { return }
+        let date: Date?
+        if let dateText = self.dateTextField.text,
+            dateText.count > 0 {
+            date = dateFormatter.date(from: dateText)
+        } else {
+            date = nil
+        }
+        let descriptionString = descriptionTextField.text
+        if let locationName = locationTextField.text,
+            locationName.count > 0 {
+            if let address = addressTextField.text,
+                address.count > 0 {
+                
+                
+                geocoder.geocodeAddressString(address) {
+                    [unowned self] (placeMarks, error) -> Void in
+                    
+                    
+                    let placeMark = placeMarks?.first
+                    
+                    
+                    let item = ToDoItem(
+                        title: titleString,
+                        itemDescription: descriptionString,
+                        timeStamp: date?.timeIntervalSince1970,
+                        location: Location(
+                            name: locationName,
+                            coordinate: placeMark?.location?.coordinate))
+                    
+                    
+                    self.itemManager?.add(item)
+                }
+            }
+        }
+    }
 }
--- ToDoTests/Controller/InputViewControllerTests.swift
+++ ToDoTests/Controller/InputViewControllerTests.swift
@@ -13,6 +13,7 @@ import CoreLocation
 class InputViewControllerTests: XCTestCase {
     
     var sut: InputViewController!
+    var placemark: MockPlacemark!
 
     override func setUp() {
         super.setUp()
@@ -67,6 +68,47 @@ class InputViewControllerTests: XCTestCase {
         let cancelButtonIsSubview = sut.cancelButton.isDescendant(of: sut.view)
         
         XCTAssertTrue(cancelButtonIsSubview)
+    }
+    
+    func test_Save_UsesGeocoderToGetCoordinateFromAddress() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        
+        let timestamp = 1456038000.0
+        let date = Date(timeIntervalSince1970: timestamp)
+        
+        sut.titleTextField.text = "Foo"
+        sut.dateTextField.text = dateFormatter.string(from: date)
+        sut.locationTextField.text = "Bar"
+        sut.addressTextField.text = "Infinite Loop 1, Cupertino"
+        sut.descriptionTextField.text = "Baz"
+        
+        let mockGeocoder = MockGeocoder()
+        sut.geocoder = mockGeocoder
+        
+        sut.itemManager = ItemManager()
+        
+        sut.save()
+        
+        placemark = MockPlacemark()
+        let coordinate = CLLocationCoordinate2D(latitude: 37.3316851, longitude: -122.0300674)
+        
+        placemark.mockCoordinate = coordinate
+        mockGeocoder.completionHandler?([placemark], nil)
+        
+        let item = sut.itemManager?.item(at: 0)
+        
+        let testItem = ToDoItem(title: "Foo", itemDescription: "Baz", timeStamp: timestamp, location: Location(name: "Bar", coordinate: coordinate))
+        
+        XCTAssertEqual(item, testItem)
+    }
+    
+    func test_SaveButtonHasSaveAction() {
+        let saveButton = sut.saveButton
+        
+        guard let actions = saveButton?.actions(forTarget: sut, forControlEvent: .touchUpInside) else { XCTFail() ; return }
+        
+        XCTAssertTrue(actions.contains("save"))
     }
 }
 
